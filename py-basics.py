b190e7ec70692d98f08e82d2501c3099653d5aa0
Sumayyah Perine <sperine01@gmail.com>
2016-09-25 16:25:20 -0400

    first commit to learning Python

---
+++ py-basics.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print(7 * 6)
+
+print()
+# ^ prints nothings to idle
+
+print('We can even include "quotes" in strings')
+# prints We can even include "quotes" in strings
+
+greeting = "Hello there"
+name = "Suma"
+
+print(greeting + " " + name)
