2525c7e1b9f8b45152f2af5a0be4adaa7145a9d8
Sumayyah Perine <sperine01@gmail.com>
2016-09-25 20:58:32 -0400

    learning inputs/variables/integers

--- py-basics.py
+++ py-basics.py
@@ -11,3 +11,18 @@ greeting = "Hello there"
 name = "Suma"
 
 print(greeting + " " + name)
+
+
+greeting = "Hello"
+name = input("Please enter your name")
+print(greeting + " " + name)
+# input one's name and output will be Hello (name)
+
+splitString = "This string has be splish \n into several \n lines"
+print(splitString)
+
+tabbedString = "We're just \t tabbing \t on em!"
+print(tabbedString)
+
+print("""She said "He loves me, he loves me not" """)
+#idle3 prints She said "He loves me, he loves me not" 
---
+++ variables.py
@@ -0,0 +1,17 @@
+# Python variables
+# variables must start with either a letter or _, no numbers
+# variables can have numbers though
+greeting = "Hello"
+_myName = "Sumayyah"
+mySaying = "Spread love it's the Brooklyn way"
+itIs2016 = "It is 2016!"
+
+# python allocates space for variables one you initialize it once you name the variable and add the =
+
+greeting = "I am"
+age = 26
+
+print(greeting + " " + age)
+# prints the error TypeError: Can't convert 'int' object to str implicitly
+
+ 
