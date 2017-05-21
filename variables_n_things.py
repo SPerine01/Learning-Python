51e8aadc372b4bfbbc9e0ba3a02d99d176d0df51
Sumayyah Perine <sperine01@gmail.com>
2017-05-21 13:14:50 -0400

    adding to my variables studies

--- variables_n_things.py
+++ variables_n_things.py
@@ -36,3 +36,34 @@ print((((a + b) / 3)- 4) * 12)
 # e = d - 4
 print(e * 12)
 # much better, 12.0
+
+# ---------
+
+bird = "Nightingale"
+print(bird)
+print(bird[2:8])
+# prints ghting
+
+print(bird[:6])
+# prints Nighti
+
+print(bird[-4:-2])
+# prints ga
+
+print(bird[0:6:3])
+# prints Nh
+
+
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+# prints 123456789
+
+print("Hello " * 5)
+# prints Hello 5x, obviously
+
+# the in operator checks if there is a substring in a string
+today = "Sunday"
+print("day" in today)
+print("Sun" in today)
+print("Thur" in today)
+print("Hello" in numbers) 
